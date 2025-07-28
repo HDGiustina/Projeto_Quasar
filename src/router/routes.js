f80be6ddb4d008abbe963ef8040e8e578c674c6d
HelenaDGiustina <helenadgiustina@gmail.com>
2025-07-28 01:55:25 -0300

    Iniciando o projeto

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,7 +3,8 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('src/pages/Dashboard/Dashboard.vue') },
+      { path: 'posts', component: () => import('src/pages/Posts/Posts.vue') }
     ]
   },
 
